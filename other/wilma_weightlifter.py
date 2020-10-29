4a14b7ecd6363cda10986fa487ea5535a2923608
Lindsay Ward <lindsay.ward@jcu.edu.au>
2020-10-30 09:42:18 +1000

    Add weightlifting demo, which includes files

---
+++ other/wilma_weightlifter.py
@@ -0,0 +1,56 @@
+"""
+CP1401 Do This Now - Live Session 27/10/2020
+
+Today's programming challenge:
+Wilma Weightlifter weally wants to keep track of her lifts
+Each gym sesh, she should enter how many lifts and the weights she lifted
+Your useful program should show her the total weight lifted
+If a lift is a PB (highest ever), she wants some serious smileys :) :)
+She don't want no bad data like negs in her stats
+She doesn't always remember her PB, so this needs to be stored between runs
+
+total = 0
+open "pb.txt" as file_in for reading
+get pb from file_in
+close file_in
+pb = ... get pb from file
+get number_of_lifts
+repeat number_of_lifts times
+    get current_weight
+    while current_weight < 0
+        print error
+        get current_weight
+    total = total + current_weight
+    if current_weight > pb
+        print smileys
+        pb = current_weight
+print total
+open "pb.txt" as file_out for writing
+write pb to file_out
+close file_out
+"""
+
+total = 0
+is_pb = False
+filename = "pb.txt"
+file_in = open(filename, "r")
+pb = float(file_in.read())
+file_in.close()
+
+number_of_lifts = int(input("Number: "))
+for i in range(number_of_lifts):
+    current_lift = float(input("Lift: "))
+    while current_lift < 0:
+        print("Error")
+        current_lift = float(input("Lift: "))
+    total += current_lift
+    if current_lift > pb:
+        print(":) :)")
+        pb = current_lift
+        is_pb = True
+    print(total)
+
+if is_pb:
+    file_out = open(filename, "w")
+    print(pb, file=file_out)
+    file_out.close()
